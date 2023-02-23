b99ddbc9df2a0aa4a1d56a3871d8bccde7645e7a
joannafelix614 <125399991+joannafelix614@users.noreply.github.com>
2023-02-23 15:57:55 -0300

    Add files via upload

--- problem_03.py
+++ problem_03.py
@@ -10,23 +10,17 @@ Inputs, Processes and Output (IPO)
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 Input(s):
 Programa Simples de Pagamento
-
 Informe quantas horas você trabalhou: 5
-
 Processes:
 Fornecida a quantidade de horas trabalhadas, calcule e forneça as informações conforme saída.
-
 valor da hora trabalhada: 25
 imposto: 0.125
 salario bruto: valor da hora trabalhada * horas trabalhadas
 impostos a cobrar: salario bruto * impostos a cobrar
 salario liquido: salario bruto - impostos a cobrar
-
 Output(s):
 Programa Simples de Pagamento
-
 Informe quantas horas você trabalhou: 5
-
 Contracheque
         Horas trabalhadas: 5
         Valor da Hora: R$25.00
@@ -37,7 +31,20 @@ Contracheque
 
 
 def main():
-    pass # your code here
+
+    print("Programa Simples de Pagamento\n")
+    horas_trabalhadas = int(input("Informe quantas horas você trabalhou: "))
+    print("\nContracheque")
+    print(f"\t\tHoras trabalhadas: {horas_trabalhadas}")
+    valor_da_hora_trabalhada = 25.00
+    print(f"\t\tValor da Hora: R${valor_da_hora_trabalhada:.2f}")
+    imposto = 0.125
+    salario_bruto = valor_da_hora_trabalhada * horas_trabalhadas
+    print(f"\t\tSalário Bruto: R${salario_bruto:.2f}")
+    imposto_a_cobrar = salario_bruto * imposto
+    print(f"\t\tImposto: R${imposto_a_cobrar:.2f}") 
+    salario_liquido = salario_bruto - imposto_a_cobrar
+    print(f"\t\tSalário Líquido: R${salario_liquido:.2f}")
 
 
 if __name__ == '__main__':
